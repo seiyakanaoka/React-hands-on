389193aa0c175850619fa2bcbaa7a617a9082ddb
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-12-19 23:44:31 +0900

    add:カウントダウンタイマー完成

--- lesson1.js
+++ lesson1.js
@@ -37,4 +37,30 @@
 //   }
 
 //   return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.ampm;
-// }; 
+// };
+
+const getNowTimes = () => {1
+  const nowTime = new Date();
+  return ({
+    year: nowTime.getFullYear(),
+    month: nowTime.getMonth(),
+    date: nowTime.getDate(),
+    hour: nowTime.getHours(),
+    minute: nowTime.getMinutes(),
+    second: nowTime.getSeconds(),
+  });
+};
+
+const clock = () => {
+  const { year, month, date, hour, minute, second } = getNowTimes();
+  console.log(`${year}/${month}/${date} ${hour}:${minute}:0${second}`);
+  return oneSecondsClear = () => {
+    console.clear();
+    clock();
+  }
+};
+
+const clockTime = (fn, mn) => setInterval(fn, mn);
+
+clockTime(clock(), 1000);
+
